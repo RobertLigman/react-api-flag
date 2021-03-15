bf91cb705c6f5f706b01a7536b69b822df967ea9
Robert Ligman <verxus123@gmail.com>
2021-03-15 12:12:47 +0100

    add basic styling for searchbar

--- src/Containers/Searchbar.js
+++ src/Containers/Searchbar.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import "./Searchbar.css";
 function SearchBar(props) {
   const [searchValue, setSearchValue] = useState("");
   const handleChange = (e) => {
@@ -7,12 +7,16 @@ function SearchBar(props) {
     setSearchValue(e.target.value);
   };
   return (
-    <div className="">
+    <div className="searchbar-form">
       <input
+        className="searchbar-input"
         placeholder="wpisz nazwę flagi ..."
         onChange={(e) => handleChange(e)}
       />
-      <button type="submit" onClick={() => props.clicked(searchValue)}>
+      <button
+        className="btn"
+        type="submit"
+        onClick={() => props.clicked(searchValue)}>
         Szukaj
       </button>
     </div>
