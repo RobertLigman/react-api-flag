1ae27ffad2213a9b967af328dc46b722673e8db0
Robert Ligman <verxus123@gmail.com>
2021-03-15 09:24:21 +0100

    add filtering flags by add searchbar component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,35 @@ import axios from "axios";
 import "./App.css";
 import { useEffect, useState } from "react";
 import Country from "./components/Country";
+import SearchBar from "./Containers/Searchbar";
 
 function App() {
-  const [peopleApi, setPeopleApi] = useState();
+  const [flagApi, setflagApi] = useState();
+  const [flagsToRender, setFlagsToRender] = useState();
   useEffect(() => {
     axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
-      setPeopleApi(res.data);
+      setflagApi(res.data);
       console.log(res.data);
     });
+    setFlagsToRender(flagApi);
   }, []);
+
+  const startSearching = (inputValue) => {
+    // console.log(e);
+    const newFlagApi = flagApi.filter((item) =>
+      item.name.toLowerCase().includes(inputValue.toLowerCase())
+    );
+    if (newFlagApi.length === 0) {
+      console.log("empty");
+    }
+    setFlagsToRender(newFlagApi);
+  };
+
   return (
     <div className="App">
-      {peopleApi &&
-        peopleApi.map((item) => (
+      <SearchBar clicked={startSearching} />
+      {flagsToRender &&
+        flagsToRender.map((item) => (
           <Country
             key={item.name}
             name={item.name}
---
+++ src/Containers/Searchbar.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function SearchBar(props) {
+  const [searchValue, setSearchValue] = useState("");
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  };
+  return (
+    <div className="">
+      <input
+        placeholder="wpisz nazwę flagi ..."
+        onChange={(e) => handleChange(e)}
+      />
+      <button type="submit" onClick={() => props.clicked(searchValue)}>
+        Szukaj
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
